1cdcc1b06f0c50c9c4d28d8f0670c05480aa7c97
Kapish Aggarwal <122765518+Kapish-Aggarwal@users.noreply.github.com>
2025-05-19 19:44:28 +0530

    Update conn.js

--- Backend/conn/conn.js
+++ Backend/conn/conn.js
@@ -1,19 +1,28 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: 'mysql', 
+    port: process.env.DB_PORT, 
+    dialect: 'mysql',
     logging: false,
-});
+    dialectOptions: {
+      connectTimeout: 20000, 
+    },
+  }
+);
 
-const conn = async () =>{
-    try{
-        await sequelize.authenticate()
-        console.log("Connection successful");
-    } catch(error){
-        console.error("Error connecting to database: ", error);
-    }
+const conn = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Connection to database successful");
+  } catch (error) {
+    console.error("❌ Error connecting to database:", error);
+  }
 };
 
 conn();
